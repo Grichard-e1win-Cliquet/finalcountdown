49aa166d0e091254b5d9fc97940b6af477ef6196
e1win <e1win@mail.ru>
2023-12-11 21:46:03 +0300

    added task

---
+++ task/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+
+Console.WriteLine("Enter some words or symbols. ");
+
+string [] Base = FillArray();
+
+string [] FillArray()
+{
+    string [] array = new string [3];
+    for(int n=0; n<array.Length; n++)
+    {
+        Console.Write($"Enter {n+1} word: ");
+        array[n] = Console.ReadLine();
+        Console.WriteLine();
+    }
+    return array;
+}
+
+string [] check(string [] array)
+{
+    int count = 0;
+    string [] less = new string [array.Length];
+    for(int n=0; n<array.Length;n++)
+    {
+        if(array[n].Length <= 3)
+        {
+            less [count] = array[n];
+            count++;
+        }
+    }
+    return less;
+}
+
+string [] result = check(Base);
+
+Console.WriteLine("Base string equals: " + '[' + string.Join(", ", Base) + ']' + ".");
+Console.WriteLine("Words including less than four symbols: " + string.Join(" ", result) + ".");
+Console.WriteLine(String.Join(" ", (result)));
